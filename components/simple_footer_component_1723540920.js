7af0beed76e23ee800eccf27b20000a7a6f8ef65
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-13 11:34:25 +0000

    Merge code from frontend

--- components/simple_footer_component_1723540920.js
+++ components/simple_footer_component_1723540920.js
@@ -2,13 +2,13 @@
 */
 Vue.component("simple_footer_component_1723540920", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-gradient-to-r from-pink-400 via-purple-500 to-blue-600 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg p-8">
+    <footer id="footer-section" style="min-height: 350px" class="flex-1 bg-gradient-to-r from-pink-400 via-purple-500 to-blue-600 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg p-8">
             <div id="footer-container" class="max-w-screen-xl mx-auto">
                 <hr id="footer-divider" class="my-6 border-white border-opacity-30">
                 <div id="footer-content" class="text-center">
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-white hover:text-pink-200 transition-colors duration-300">
-                            <img id="footer-logo" src="./images/logo.svg" class="h-9 mr-3 filter brightness-0 invert" alt="Landwind Logo" />
+                            <img id="footer-logo" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/tmplh4w3dd5/logo.svg" class="h-9 mr-3 filter brightness-0 invert" alt="Landwind Logo" />
                             FyrExt: Streamlining Fire Safety
                         </a>
                     </div>
